46cc7a0563311d7d5327ecccf5ba679eacc90c30
drjohnrussell <russellj@gmail.com>
2025-04-02 09:30:13 -0400

    challenge 2

---
+++ 2025/Challenge02.R
@@ -0,0 +1,30 @@
+library(HistData)
+library(tidyverse)
+library(RColorBrewer)
+
+data("Galton")
+data("PearsonLee")
+
+Galton |> 
+  ggplot(aes(x=parent,y=child)) +
+  geom_point(position="jitter") +
+  geom_density2d_filled(alpha=.6,show.legend=FALSE,bins=9) +
+  theme_minimal() +
+  geom_smooth(method="loess",se=FALSE,color="red") +
+  geom_smooth(method="lm",se=FALSE) +
+  labs(title="Galton's Height Data",
+       x="Parent Height (inches)",
+       y="Child Height (inches)") +
+  scale_fill_brewer(type="seq")
+
+
+PearsonLee |> 
+  ggplot(aes(x=parent,y=child)) +
+  geom_point() +
+  theme_minimal() +
+  geom_smooth(aes(weight=frequency), method="lm", se=FALSE, color="red",size=2) +
+  geom_smooth(aes(weight = frequency), method="lm", se=FALSE) +
+  facet_grid(par ~ chl) +
+  labs(title="Pearson and Lee's Data on Parent and Child",
+       x="Parent Height (inches)",
+       y="Child Height (inches)")
