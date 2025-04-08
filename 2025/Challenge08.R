acfefd2da628968e6049081040a61e0a0e15e75b
drjohnrussell <russellj@gmail.com>
2025-04-08 07:02:57 -0400

    Challenge 8

---
+++ 2025/Challenge08.R
@@ -0,0 +1,20 @@
+library(HistData)
+library(tidyverse)
+library(RColorBrewer)
+
+
+data("Pyx")
+
+Pyx |> 
+  mutate(Bags=fct_relevel(Bags,"5","6","7")) |>
+  group_by(Bags) |> 
+  mutate(percent=count/sum(count)*100) |>
+  ungroup() |>
+  ggplot(aes(x=Deviation,y=percent,group=Bags,fill=Group)) +
+  geom_col(stat="identity",position=position_dodge()) +
+  theme_minimal() +
+  theme(legend.position = "top") +
+  labs(x="Deviation from the Standard",
+       y="Percentage of an individual bag",
+       title="Trial of the Pyx (1848)",
+       fill="")
