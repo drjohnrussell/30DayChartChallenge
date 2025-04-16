652ec83ee464e802d5bb0c8a9906520dc93aaa04
drjohnrussell <russellj@gmail.com>
2025-04-16 06:08:59 -0400

    update

--- 2025/Challenge16.R
+++ 2025/Challenge16.R
@@ -6,7 +6,8 @@ data("HalleyLifeTable")
 HalleyLifeTable |> 
   ggplot(aes(xmin=number,ymin=age-1,ymax=age-.1,xmax=0)) +
   geom_rect() +
-  labs(x="Persons Living",
+  labs(x="Persons Living Through this Age",
        y="Age",
        title="Halley's Life Table",
-       subtitle="Breslau")
+       subtitle="Breslau") +
+  theme_minimal()
