2f2b33bb9273e353ee907316ca1ef712cc112ebd
drjohnrussell <97848733+drjohnrussell@users.noreply.github.com>
2025-04-18 11:13:09 -0400

    Changed Challenge 18

--- 2025/Challenge18.R
+++ 2025/Challenge18.R
@@ -10,7 +10,13 @@ data2 <- st_as_sf(gfrance)
 
 data2 |> 
   ggplot() +
-  geom_sf(aes(geometry=geometry,fill=Instruction)) +
-  geom_sf_text(aes(label=Instruction,color=(Instruction<37)),size=2) +
+  geom_sf(aes(geometry=geometry,fill=Instruction),show.legend=FALSE) +
+  geom_sf_text(aes(label=Instruction,color=(Instruction<37)),size=2,
+               show.legend=FALSE) +
   scale_fill_gradient2(low = "white", high = 'black') +
-  scale_color_manual(values=c("white","black"))
+  scale_color_manual(values=c("white","black")) +
+  theme_void() +
+  labs(
+    title = "Guerry's data on French Instruction (1833)",
+    subtitle="Ranking based on ability of conscripts to read and write",
+    caption="Source: Guerry and HistData packages")
